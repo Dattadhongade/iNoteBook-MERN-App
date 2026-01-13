b21d1a56c3e89444735f854a775746fa20899a8f
Datta Dhongade <dhongadedattatray12@gmail.com>
2026-01-13 21:43:12 +0530

    connected  to database

---
+++ backend/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const mongoURL = "mongodb://localhost:27017/";
+const connectToMongo = async () => {
+  try {
+   await mongoose.connect(mongoURL);
+    console.log("MongoDb connected successfully");
+  } catch {
+    console.error(" MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+module.exports = connectToMongo;
